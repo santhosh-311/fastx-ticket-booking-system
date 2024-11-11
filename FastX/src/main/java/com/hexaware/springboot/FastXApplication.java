9887d9dcf34034bc44beadf7f14b92d5af1ab42e
hp <hp@Santhosh>
2024-11-11 16:52:27 +0530

    first commit

---
+++ FastX/src/main/java/com/hexaware/springboot/FastXApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastXApplication.class, args);
+	}
+
+}
---
+++ FastX/src/test/java/com/hexaware/springboot/FastXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
